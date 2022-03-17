6d91ba5ff95509d8cf342f4f90b30ca21c4da0b2
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2022-03-17 15:43:38 -0300

    Requisito 4 sem erros de lint

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  return data.employees.some((element) => element.managers.includes(id));
+  return employees.some((func) => func.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return employees
+      .filter((element) => element.managers.includes(managerId))
+      .map((element) => `${element.firstName} ${element.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
