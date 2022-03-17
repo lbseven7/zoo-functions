204277b3b5f80f0bcd916fbe6bb4dbc015e53321
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2022-03-17 14:35:36 -0300

    Mais erros de lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,5 @@
 const { employees } = require('../data/zoo_data');
-// const data = require('../data/zoo_data');
-
-function getEmployeeByName(employeeName) { // busca empregado pelo nome
+function getEmployeeByName(employeeName) {
   const empregado = employees.find((element) => element.firstName === employeeName || element.lastName === employeeName);
   return empregado === undefined ? {} : empregado;
 }
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,8 +1,9 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((element) => element.managers.includes(id))
 }
+console.log(isManager());
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
