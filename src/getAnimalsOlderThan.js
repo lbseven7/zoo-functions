a8c40092dbd4616e5f6f48f0b0a9e434a7d98c29
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2022-03-17 14:12:50 -0300

    Consertando erros de lint src/getAnimalsOlderThan.js

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,9 +1,10 @@
-const { species } = require('../data/zoo_data');
+const { species } = require("../data/zoo_data");
 // const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const animalName = species.find((element) => element.name === animal)
-  .residents.every((element) => element.age >= age);;
+  const animalName = species
+    .find((element) => element.name === animal)
+    .residents.every((element) => element.age >= age);
 
   return animalName;
 }
