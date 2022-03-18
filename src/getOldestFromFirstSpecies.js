4898c511eef4c78f21446a51097b48d33ca1eb00
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2022-03-18 11:30:25 -0300

    Requisito 9 getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const responsavel = data.employees.find((element) => element.id === id)
+    .responsibleFor[0]; // encontrar o id do responsável por
+
+  const animalMaisVelho = data.species.find(
+    (element) => element.id === responsavel,
+  ).residents; // encontar o animal mais velho
+
+  const animalArray = animalMaisVelho.reduce(
+    (acc, element) => {
+      if (element.age > acc[2]) {
+        return [element.name, element.sex, element.age]; // retorna animal[0], sexo[1] e idade[2] num array
+      }
+      return acc;
+    },
+    [undefined, undefined, 0],
+  ); // undefined[0], undefined[1], 0[2] (valor inicial do reduce)
+  return animalArray;
 }
 
 module.exports = getOldestFromFirstSpecies;
