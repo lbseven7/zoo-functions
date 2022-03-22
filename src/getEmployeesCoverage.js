5b63f2d578dd9ad91f9e00aba7efa0051be86d07
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2022-03-22 12:35:08 -0300

    Requisito 10 primeira funçao ok

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,17 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+// busca empregado por id
+function encontraEmpregadoPorId(id) {
+  const encontrado = employees.find((element) => element.id === id);
+  if (!encontrado) {
+    throw new Error('Informações inválidas');
+  }
+  return encontrado;
+}
+
+function getEmployeesCoverage(employee) {
+
 }
 
 module.exports = getEmployeesCoverage;
