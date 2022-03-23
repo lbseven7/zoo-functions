86e23007afa604a103788aba0eb6677cd62d0414
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2022-03-23 12:09:25 -0300

    Requisito 5 countAnimals erros de lintgit add src/countAnimals.jsgit add src/countAnimals.js

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,12 +2,22 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   const objeto = {};
+  // testa
   if (!animal) {
     data.species.forEach((element) => {
       objeto[element.name] = element.residents.length;
     });
     return objeto;
   }
-}
 
+  if (!animal.sex) {
+    return data.species
+      .filter((element) => element.name === animal.specie)[0]
+      .residents.length;
+  }
+  return data.species
+    .filter((element) => element.name === animal.specie)[0]
+    .residents.filter((element) => element.sex === animal.sex).length;
+}
+console.log(countAnimals({ specie: 'LIONS'.toLowerCase(), sex: 'male' }));
 module.exports = countAnimals;
